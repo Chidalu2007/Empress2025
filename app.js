8b541276c4f2e79d5251a8bece71b706eda8c8da
Ugwu Chidalu <chidaludivinefavour72@gmail.com>
2025-01-28 12:12:05 +0100

    nice one

---
+++ app.js
@@ -0,0 +1,21 @@
+// let numbers= [1,2,3,4,5,6,7,8,9,10];
+// function sum (acc,curr){
+//     return acc + curr;
+// }
+// let sumT =numbers.reduce(sum)
+
+// console.log(sumT);
+
+// function numberFilterer(num) {
+//     return num < 5;
+// }
+// let filteredNum = numbers.filter(numberFilterer)
+// console.log(filteredNum);
+
+
+// function mappedNum(num) {
+//     return num + 2;
+// }
+// let mappedNumbers = numbers.map(mappedNum)
+// console.log(mappedNumbers);
+
